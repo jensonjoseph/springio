1076d75954bdcc8718433d0049446f843869696e
Kakkattil, Jenson Jos <jenson.kakkattil@ge.com>
2018-01-13 16:18:43 -0500

    Initial commit

---
+++ src/main/java/com/jensonjo/Application.java
@@ -0,0 +1,10 @@
+package com.jensonjo;
+
+/**
+ * Created by jensonkakkattil on 1/13/18.
+ */
+public class Application {
+    public static void main(String args[]) {
+        System.out.print("Hello World");
+    }
+}
